5fedd5afa83ad94bcfe117941bb0ecc25aa872f2
Isa-dlc15 <deleonisaias13@gmail.com>
2024-07-14 20:58:29 -1200

    Ya es el quinto intento :(

---
+++ PrincipiosDePoo/EstudianteYaInscritoException.java
@@ -0,0 +1,8 @@
+package PrincipiosDePoo;
+
+public class EstudianteYaInscritoException extends Exception {
+    public EstudianteYaInscritoException(String message) {
+        super(message);
+    }
+}
+
